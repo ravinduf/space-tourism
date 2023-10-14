fccbc9fb48ca070c7f3ded1a1d899c1f342c0953
ravinduf <ravinduf@rocketmail.com>
2023-10-14 10:53:36 +0530

    added NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav class="flex flex-row items-center justify-between h-[90px]">
+    <img class="h-[48px] w-[48px]" src="../assets/shared/logo.svg"/>
+    
+    <div class="flex flex-row justify-around w-[50vw] h-[90px] px-4 bg-[#979797]/20">
+      <div class="link link--active">
+        <span class="link__row">
+          <span class="link--bold">00</span> Home
+        </span>
+      </div>
+      <div class="link link--active">
+        <span class="link__row">
+          <span class="link--bold">01</span> Destination
+        </span>
+      </div>
+      <div class="link link--active">
+        <span class="link__row">
+          <span class="link--bold">02</span> Destination
+        </span>
+      </div>
+      <div class="link lg:mr-[150px] md:mr-[30px] link--active">
+        <span class="link__row">
+          <span class="link--bold">03</span> Technology
+        </span>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.link {
+  @apply text-white text-base font-barlow h-full flex flex-col justify-center  
+}
+
+.link--active {
+  @apply hover:border-b-2 hover:border-b-white active:border-b-2 active:border-b-white
+}
+.link__row {
+  @apply flex flex-row justify-between;
+}
+.link--bold {
+  @apply font-bold mr-1
+}
+
+</style>
