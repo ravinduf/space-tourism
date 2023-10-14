0bd0e61eb22dcc49c7dc87b0c8e9c7e5494b568c
ravinduf <ravinduf@rocketmail.com>
2023-10-14 10:53:10 +0530

    added basic bg color

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
+  <NavBar />
   <RouterView/>
 </template>
 
