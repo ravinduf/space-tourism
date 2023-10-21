24d55e0965c6c13654eeb5e80048f107b0658fcf
ravinduf <ravinduf@rocketmail.com>
2023-10-21 19:48:07 +0530

    home page done

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,19 +1,23 @@
 <template>
   <div class="body"></div>
 
-  <div class="mx-auto w-4/5 md:w-1/2 text-center mt-4 md:mt-20">
-    <div class=" text-base md:text-2xl font-barlow text-primaryLightBlue tracking-widest uppercase">So, you want to travel to</div>
-    <div class="text-7xl md:text-[150px] font-bellefair uppercase my-4 md:my-5">Space</div>
-    <div class="font-barlow text-[15px] md:text-base text-primaryLightBlue md:tracking-widest">Let’s face it; if you want to go to space, you might as well genuinely go to 
-    outer space and not hover kind of on the edge of it. Well sit back, and relax 
-    because we’ll give you a truly out of this world experience!</div>
+  <div class="flex flex-col items-center lg:flex-row lg:justify-between lg:w-[75%] lg:mx-auto mt-4 md:mt-20 lg:mt-[30vh]">
+    <div class="w-4/5 md:w-[60%] lg:w-[36%] text-center lg:text-left ">
+      <div class="text-base md:text-2xl lg:text-3xl font-barlow text-primaryLightBlue tracking-widest uppercase">So, you want to
+        travel to</div>
+      <div class="text-7xl md:text-[150px] font-bellefair uppercase my-4 md:my-5">Space</div>
+      <div class="font-barlow text-[15px] lg:text-lg md:text-base text-primaryLightBlue md:tracking-widest">Let’s face it; if you
+        want to go to space, you might as well genuinely go to
+        outer space and not hover kind of on the edge of it. Well sit back, and relax
+        because we’ll give you a truly out of this world experience!</div>
+    </div>
 
-  </div>
-
-  <div class="mt-[160px] md:mt-[220px] mx-auto w-[150px] h-[150px] md:w-[242px] md:h-[242px] bg-white text-[#0B0D17] flex flex-col font-bellefair text-2xl uppercase justify-center items-center rounded-full">
-    <span>
-      Explore
-    </span>
+    <div
+      class="mt-[160px] md:mt-[220px] lg:mt-10 w-[150px] h-[150px] md:w-[242px] md:h-[242px] lg:h-[274px] lg:w-[274px] bg-white text-[#0B0D17] flex flex-col font-bellefair text-2xl lg:text-3xl uppercase justify-center items-center rounded-full">
+      <span>
+        Explore
+      </span>
+    </div>
   </div>
 </template>
 
@@ -27,8 +31,6 @@
   left: 0;
   z-index: -2;
 
-  @apply bg-home-mobile md:bg-home-tablet lg:bg-home-desktop bg-cover bg-bottom 
+  @apply bg-home-mobile md:bg-home-tablet lg:bg-home-desktop bg-cover lg:bg-center
 }
-
-
 </style>
