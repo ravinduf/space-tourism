0a32230d0925a82826c032bc6f6913ede38c9449
ravinduf <ravinduf@rocketmail.com>
2023-10-21 19:07:10 +0530

    basic page for mobile and tab done

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,3 +1,34 @@
 <template>
-  <h1>Home page</h1>
+  <div class="body"></div>
+
+  <div class="mx-auto w-4/5 md:w-1/2 text-center mt-4 md:mt-20">
+    <div class=" text-base md:text-2xl font-barlow text-primaryLightBlue tracking-widest uppercase">So, you want to travel to</div>
+    <div class="text-7xl md:text-[150px] font-bellefair uppercase my-4 md:my-5">Space</div>
+    <div class="font-barlow text-[15px] md:text-base text-primaryLightBlue md:tracking-widest">Let’s face it; if you want to go to space, you might as well genuinely go to 
+    outer space and not hover kind of on the edge of it. Well sit back, and relax 
+    because we’ll give you a truly out of this world experience!</div>
+
+  </div>
+
+  <div class="mt-[160px] md:mt-[220px] mx-auto w-[150px] h-[150px] md:w-[242px] md:h-[242px] bg-white text-[#0B0D17] flex flex-col font-bellefair text-2xl uppercase justify-center items-center rounded-full">
+    <span>
+      Explore
+    </span>
+  </div>
 </template>
+
+<style scoped>
+.body {
+  /* background-image: url("../assets/home/background-home-desktop.jpg"); */
+  height: 100vh;
+  width: 100vw;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -2;
+
+  @apply bg-home-mobile md:bg-home-tablet lg:bg-home-desktop bg-cover bg-bottom 
+}
+
+
+</style>
