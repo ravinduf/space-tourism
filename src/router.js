da94139ecd81a20ec48a9a00577be5a2fa7cae72
ravinduf <ravinduf@rocketmail.com>
2023-11-04 10:38:42 +0530

    created destinations.js file renamed img files

--- src/router.js
+++ src/router.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
 import Home from "./pages/Home.vue"
-import Destination from "./pages/Destination.vue"
+import Destination from "./pages/Destination/Destination.vue"
 import Crew from "./pages/Crew.vue"
 import Technology from "./pages/Technology.vue"
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/destination', component: Destination },
+  { path: '/destination', component: Destination, },
   { path: '/crew', component: Crew },
   { path: '/technology', component: Technology }
 ]
