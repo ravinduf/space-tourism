0627c52231719a2a7b2a287a960e0a259b0687d5
ravinduf <ravinduf@rocketmail.com>
2023-11-04 12:54:42 +0530

    added bg images and created the heading

--- src/pages/Destination/Destination.vue
+++ src/pages/Destination/Destination.vue
@@ -1,3 +1,19 @@
 <template>
-  <h1>Destination</h1>
+  <div class="body"></div>
+    <div class="uppercase font-barlow text-base text-center mt-3">
+      <span class="text-[#4d5056]">01</span> pick your destination
+    </div>
 </template>
+
+<style scoped>
+.body {
+  height: 100vh;
+  width: 100vw;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -2;
+
+  @apply bg-destination-mobile md:bg-destination-tablet lg:bg-destination-desktop bg-cover lg:bg-center
+}
+</style>
