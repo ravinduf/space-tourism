bbbe4d44f0ca8b0c9285e1191aabf54373050ccb
ravinduf <ravinduf@rocketmail.com>
2023-11-04 21:39:01 +0530

    updated destinations to have key field

--- src/pages/Destination/destinations.js
+++ src/pages/Destination/destinations.js
@@ -3,8 +3,9 @@ import mars from "../../assets/destination/mars.webp"
 import europa from "../../assets/destination/europa.webp"
 import titan from "../../assets/destination/titan.webp"
 
-const destinations = {
+export const destinations = {
   moon: {
+    key: 'moon',
     description: `See our planet as you’ve never seen it before. A perfect relaxing trip away to help 
     regain perspective and come back refreshed. While you’re there, take in some history 
     by visiting the Luna 2 and Apollo 11 landing sites.`,
@@ -13,6 +14,7 @@ const destinations = {
     img: moon
   },
   mars: {
+    key: 'mars',
     description: `Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, 
     the tallest planetary mountain in our solar system. It’s two and a half times 
     the size of Everest!`,
@@ -21,6 +23,7 @@ const destinations = {
     img: mars
   },
   europa: {
+    key: 'europa',
     description: `The smallest of the four Galilean moons orbiting Jupiter, Europa is a 
     winter lover’s dream. With an icy surface, it’s perfect for a bit of 
     ice skating, curling, hockey, or simple relaxation in your snug 
@@ -30,6 +33,7 @@ const destinations = {
     img: europa
   },
   titan: {
+    key: 'titan',
     description: `The only moon known to have a dense atmosphere other than Earth, Titan 
     is a home away from home (just a few hundred degrees colder!). As a 
     bonus, you get striking views of the Rings of Saturn.`,
