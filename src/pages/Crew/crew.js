6601814c121425d46fd5654cd721f16964cc10c8
ravinduf <ravinduf@rocketmail.com>
2023-11-05 15:36:49 +0530

    updated

--- src/pages/Crew/crew.js
+++ src/pages/Crew/crew.js
@@ -10,7 +10,8 @@ export const crew = {
     name: "mark shuttleworth",
     description: ` Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind 
     the Linux-based Ubuntu operating system. Shuttleworth became the first South 
-    African to travel to space as a space tourist.`
+    African to travel to space as a space tourist.`,
+    img: specialist
   },
   engineer: {
     key: 'engineer',
@@ -18,14 +19,16 @@ export const crew = {
     name: 'anousheh ansari',
     description: `Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. 
     Ansari was the fourth self-funded space tourist, the first self-funded woman to 
-    fly to the ISS, and the first Iranian in space.`
+    fly to the ISS, and the first Iranian in space.`,
+    img: engineer
   },
   commander: {
     key: 'commander',
     designation: 'dougles hurley',
     description: `Douglas Gerald Hurley is an American engineer, former Marine Corps pilot 
     and former NASA astronaut. He launched into space for the third time as 
-    commander of Crew Dragon Demo-2.`
+    commander of Crew Dragon Demo-2.`,
+    img: commander
   },
   pilot: {
     key: 'pilot',
@@ -34,7 +37,8 @@ export const crew = {
     description: `Pilot on the first operational flight of the SpaceX Crew Dragon to the 
     International Space Station. Glover is a commander in the U.S. Navy where 
     he pilots an F/A-18.He was a crew member of Expedition 64, and served as a 
-    station systems flight engineer.`
+    station systems flight engineer.`,
+    img: pilot
   }
 
 }
